a9fe753ba315393c9f952753389cfd2e8d042e83
Luiz Aires Soares <lfelipeas@gmail.com>
2022-04-04 16:26:46 -0400

    Sets up project structure

---
+++ valet-challenge/valet-challengeUITests/valet_challengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  valet_challengeUITestsLaunchTests.swift
+//  valet-challengeUITests
+//
+//  Created by Luiz Felipe Aires Soares on 2022-03-31.
+//
+
+import XCTest
+
+class valet_challengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
